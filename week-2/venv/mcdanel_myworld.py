03f034cd315c422170e6a4941944737644644f73
Kayla McDanel <ks.mcdanel@gmail.com>
2022-08-18 11:02:28 -0400

    Initial commit

---
+++ week-2/venv/mcdanel_myworld.py
@@ -0,0 +1,8 @@
+#
+#    Title: mcdanel_myworld.py
+#   Author: Kayla McDanel
+#   Date: 18 August 2022
+#   Description: Example file for outputting values.
+
+last_name = "McDanel"
+print('You are now in ' + last_name + '\'s world!')

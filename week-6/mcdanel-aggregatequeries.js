4ece4450ba4c1e60677e3df2bb227af247231308
Kayla McDanel <ks.mcdanel@gmail.com>
2022-09-16 09:28:19 -0400

    comments

---
+++ week-6/mcdanel-aggregatequeries.js
@@ -0,0 +1,4 @@
+    // Title: mcdanel-aggregatequeries.js
+    // Author: Kayla McDanel
+    // Date: 09 16 2022
+    // Description: Aggregate queries.
--- week-6/mcdanel_usersp1.py
+++ week-6/mcdanel_usersp1.py
@@ -13,9 +13,12 @@ client = MongoClient ("mongodb+srv://web335_user:s3cret@web335db.n31cdf0.mongodb
 # Configure a variable to access the web335DB
 db = client ['web335DB']
 
+# Prints all users in the collection
 for user in db.users.find({}, {"firstName": 1, "lastName":1 }):
     print(user)
 
+# Prints employee info for person with ID number 1011
 print(db.users.find_one({"employeeId":"1011"}))
 
+# Prints employee info for person with last name Mozart
 print(db.users.find_one({"lastName":"Mozart"}))
